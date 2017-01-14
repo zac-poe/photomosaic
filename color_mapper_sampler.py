380fe5381435e0b4d86151a85146e8ec7a91ecef
Zac Poe <zacharypoe@gmail.com>
2017-01-14 10:08:11 -0500

    added black & white text for readability

--- color_mapper_sampler.py
+++ color_mapper_sampler.py
@@ -5,8 +5,11 @@ from color_mapper import ColorMapper
 mapper = ColorMapper()
 
 print("<html>")
-print("<style>table td { border:1px solid black; text-align:center }</style>")
-print("<table cellpadding=25>")
+print("<style>"
+    "table td { border:1px solid black; text-align:center; height: 90px; width: 100px; }"
+    "div { color: #ffffff; }"
+    "</style>")
+print("<table>")
 
 values = [0, 64, 128, 191, 255]
 
@@ -15,8 +18,9 @@ for r in values:
         print("<tr>")
         for b in values:
             print("<td style='background-color: rgb(" + str(r) + "," + str(g) + "," + str(b) + ")'>" 
-                    + mapper.map([r,g,b]) 
-                    + "<br/>" + str(r) + "-" + str(g) + "-" + str(b) + "</td>")
+                    + mapper.map([r,g,b]) + "<br>" + str(r) + "-" + str(g) + "-" + str(b)
+                    + "<div>" + mapper.map([r,g,b]) + "<br>" + str(r) + "-" + str(g) + "-" + str(b) + "</div>"
+                    + "</td>")
         print("</tr>")
 
 print("</table>")
