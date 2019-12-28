96ec4fa6689861678053e5ac8cd75eb55fd66b7b
zac-poe <zacharypoe@gmail.com>
2019-12-28 18:08:52 -0500

    adding untile method to go from a mosaic back to an image

--- mosaic.py
+++ mosaic.py
@@ -5,7 +5,7 @@ import collections
 class Mosaic:
     # divides a nested list of pixels into a nested list of mosaic tiles
     # containing the respective x-by-y lists of pixels
-    def tile(self, pixels, xy=(10,10)):
+    def tile(self, pixels, xy):
         if not isinstance(pixels, list) or not isinstance(pixels[0], list):
             raise ValueError("Provided pixels must be a nested list")
         if not isinstance(xy, collections.Sequence) or len(xy) < 2 \
@@ -35,3 +35,43 @@ class Mosaic:
             if (x+1) % xy[0] == 0:
                 tileX += 1
         return tiles
+
+    # joins a nested list of mosaic tile pixel lists into a single nested pixel list
+    def untile(self, tiles):
+        pixels = []
+        if not isinstance(tiles, list) or not isinstance(tiles[0], list):
+            raise ValueError("Provided tiles must be a nested list")
+        maxTileY = len(tiles[0])
+
+        absoluteX = -1  # allows for increment every iteration
+        for tileX in range(0, len(tiles)):
+            absoluteY = -1  # allows for increment every iteration
+            prevAbsoluteX = absoluteX
+            for tileY in range(0, maxTileY):
+                absoluteX = prevAbsoluteX
+                if(maxTileY != len(tiles[tileX])):
+                    raise ValueError("Uneven tiles list provided")
+                if not isinstance(tiles[tileX][tileY], list):
+                    raise ValueError("Tile at {0},{1} is not a pixel list"
+                        .format(tileX, tileY))
+                prevAbsoluteY = absoluteY
+                for x in range(0, len(tiles[tileX][tileY])):
+                    if not isinstance(tiles[tileX][tileY][x], list):
+                        raise ValueError("Tile at {0},{1} is not a nested pixel list"
+                            .format(tileX, tileY))
+                    absoluteY = prevAbsoluteY
+                    absoluteX += 1
+                    if absoluteX >= len(pixels):
+                        pixels.append([])
+                    for y in range(0, len(tiles[tileX][tileY][x])):
+                        absoluteY += 1
+                        if absoluteY >= len(pixels[absoluteX]):
+                            pixels[absoluteX].append([])
+                        pixels[absoluteX][absoluteY] = tiles[tileX][tileY][x][y]
+
+        maxY = len(pixels[0])
+        for i in range(0, len(pixels)):
+            if len(pixels[i]) != maxY:
+                raise ValueError("Tiles contain uneven pixel lists")
+
+        return pixels
--- test/mosaic_test.py
+++ test/mosaic_test.py
@@ -105,9 +105,9 @@ def test_tile_with_remainder_3_by_3_as_2_by_2():
 
 def test_tile_uneven_descending():
     pixels = [
-        [(0,0,0),(0,0,0),(0,0,0)],
-        [(0,0,0),(0,0,0)],
-        [(0,0,0)]
+        [RED,RED,RED],
+        [RED,RED],
+        [RED]
     ]
     try:
         testSubject.tile(pixels, (1,1))
@@ -117,9 +117,9 @@ def test_tile_uneven_descending():
 
 def test_tile_uneven_ascending():
     pixels = [
-        [(0,0,0)],
-        [(0,0,0),(0,0,0)],
-        [(0,0,0),(0,0,0),(0,0,0)]
+        [RED],
+        [RED,RED],
+        [RED,RED,RED]
     ]
     try:
         testSubject.tile(pixels, (1,1))
@@ -127,15 +127,15 @@ def test_tile_uneven_ascending():
     except ValueError:
         pass
 
-def test_write_pixels_bad_pixels_argument():
+def test_tile_bad_pixels_argument():
     try:
         testSubject.tile("pixels", (1,1))
         raise RuntimeError("expected ValueError")
     except ValueError:
         pass
 
-def test_write_pixels_non_nested_pixels():
-    pixels = [(0,0,0),(0,0,0)]
+def test_tile_non_nested_pixels():
+    pixels = [RED,RED]
     try:
         testSubject.tile(pixels, (1,1))
         raise RuntimeError("expected ValueError")
@@ -146,8 +146,8 @@ def test_tile_bad_tile_dimensions_argument():
     dimensions = "dimensions"
     try:
         testSubject.tile([
-                [(0,0,0),(0,0,0)],
-                [(0,0,0),(0,0,0)]
+                [RED,RED],
+                [RED,RED]
             ], dimensions)
         raise RuntimeError("expected ValueError")
     except ValueError:
@@ -157,8 +157,8 @@ def test_tile_no_tile_dimensions():
     dimensions = ()
     try:
         testSubject.tile([
-                [(0,0,0),(0,0,0)],
-                [(0,0,0),(0,0,0)]
+                [RED,RED],
+                [RED,RED]
             ], dimensions)
         raise RuntimeError("expected ValueError")
     except ValueError:
@@ -168,8 +168,8 @@ def test_tile_missing_y_tile_dimension():
     dimensions = (5)
     try:
         testSubject.tile([
-                [(0,0,0),(0,0,0)],
-                [(0,0,0),(0,0,0)]
+                [RED,RED],
+                [RED,RED]
             ], dimensions)
         raise RuntimeError("expected ValueError")
     except ValueError:
@@ -179,8 +179,8 @@ def test_tile_non_numeric_x_tile_dimension():
     dimensions = ("a",5)
     try:
         testSubject.tile([
-                [(0,0,0),(0,0,0)],
-                [(0,0,0),(0,0,0)]
+                [RED,RED],
+                [RED,RED]
             ], dimensions)
         raise RuntimeError("expected ValueError")
     except ValueError:
@@ -190,9 +190,252 @@ def test_tile_non_numeric_y_tile_dimension():
     dimensions = (5,"a")
     try:
         testSubject.tile([
-                [(0,0,0),(0,0,0)],
-                [(0,0,0),(0,0,0)]
+                [RED,RED],
+                [RED,RED]
             ], dimensions)
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+# In           Out
+# RR GG BB ==> RRGGBB
+# RR GG BB     RRGGBB
+def test_untile_to_6_by_2():
+    tiles = [
+        [
+            [
+                [RED, RED],
+                [RED, RED]
+            ]
+        ],
+        [
+            [
+                [GREEN, GREEN],
+                [GREEN, GREEN]
+            ]
+        ],
+        [
+            [
+                [BLUE, BLUE],
+                [BLUE, BLUE]
+            ]
+        ]
+    ]
+    pixels = testSubject.untile(tiles)
+    assert len(pixels) == 6
+    for x in range(0, len(pixels)):
+        assert len(pixels[x]) == 2
+        for y in range (0, len(pixels[x])):
+            if x < 2:
+                color = RED
+            elif x < 4:
+                color = GREEN
+            else:
+                color = BLUE
+            assert pixels[x][y] == color
+
+# In       Out
+# BB       BB
+# BB       BB
+#          GG
+# GG  ==>  GG
+# GG       RR
+#          RR
+# RR
+# RR
+def test_untile_to_2_by_6():
+    tiles = [
+        [
+            [
+                [RED, RED],
+                [RED, RED]
+            ],
+            [
+                [GREEN, GREEN],
+                [GREEN, GREEN]
+            ],
+            [
+                [BLUE, BLUE],
+                [BLUE, BLUE]
+            ]
+        ]
+    ]
+    pixels = testSubject.untile(tiles)
+    assert len(pixels) == 2
+    for x in range(0, len(pixels)):
+        assert len(pixels[x]) == 6
+        for y in range (0, len(pixels[x])):
+            if y < 2:
+                color = RED
+            elif y < 4:
+                color = GREEN
+            else:
+                color = BLUE
+            assert pixels[x][y] == color
+
+# In       Out
+# BB B      BBB
+#      ==>  GGG
+# GG G      RRR
+# RR R
+def test_untile_uneven_to_3_by_3():
+    tiles = [
+        [
+            [
+                [RED, GREEN],
+                [RED, GREEN]
+            ],
+            [
+                [BLUE],
+                [BLUE]
+            ]
+        ],
+        [
+            [
+                [RED, GREEN]
+            ],
+            [
+                [BLUE]
+            ]
+        ]
+    ]
+    pixels = testSubject.untile(tiles)
+    assert len(pixels) == 3
+    for x in range(0, len(pixels)):
+        assert len(pixels[x]) == 3
+        for y in range (0, len(pixels[x])):
+            if y == 0:
+                color = RED
+            elif y == 1:
+                color = GREEN
+            else:
+                color = BLUE
+            assert pixels[x][y] == color
+
+def test_untile_bad_tiles_argument():
+    try:
+        testSubject.untile("tiles")
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+def test_untile_non_nested_tiles():
+    tiles = [RED,RED]
+    try:
+        testSubject.untile(tiles)
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+def test_untile_uneven_descending():
+    tiles = [
+        [
+            [
+                [RED]
+            ],
+            [
+                [RED]
+            ],
+            [
+                [RED]
+            ]
+        ],
+        [
+            [
+                [RED]
+            ],
+            [
+                [RED]
+            ]
+        ],
+        [
+            [
+                [RED]
+            ]
+        ]
+    ]
+    try:
+        testSubject.untile(tiles)
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+def test_untile_uneven_ascending():
+    tiles = [
+        [
+            [
+                [RED]
+            ]
+        ],
+        [
+            [
+                [RED]
+            ],
+            [
+                [RED]
+            ]
+        ],
+        [
+            [
+                [RED]
+            ],
+            [
+                [RED]
+            ],
+            [
+                [RED]
+            ]
+        ]
+    ]
+    try:
+        testSubject.untile(tiles)
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+def test_untile_bad_tile():
+    tiles = [
+        [
+            "tile"
+        ]
+    ]
+    try:
+        testSubject.untile(tiles)
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+def test_untile_non_nested_tile():
+    tiles = [
+        [
+            [
+                "tile"
+            ]
+        ]
+    ]
+    try:
+        testSubject.untile(tiles)
+        raise RuntimeError("expected ValueError")
+    except ValueError:
+        pass
+
+def test_untile_uneven_tile_pixel_columns():
+    tiles = [
+        [
+            [
+                [RED, RED],
+                [RED]
+            ]
+        ],
+        [
+            [
+                [RED, RED],
+                [RED, RED]
+            ]
+        ]
+    ]
+    try:
+        testSubject.untile(tiles)
         raise RuntimeError("expected ValueError")
     except ValueError:
         pass
